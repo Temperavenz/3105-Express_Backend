fb5e36f78eb9f7f05cfb15b481546fff8d03e8f7
Temperavenz <22103492@usc.edu.ph>
2024-10-06 00:51:47 +0800

    Made custom changes on the messages of both RLM and AM files inside the middleware folder

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken');
-const secretKey = 'your-secret-key'; // Use environment variables for production
+const secretKey = 'your-secret-key';
 
 module.exports = (req, res, next) => {
   const token = req.header('Authorization');
-  if (!token) return res.status(401).send('Access denied. No token provided.');
+  if (!token) return res.status(401).send('Dota 2 hero voice: DENIED. No token provided.');
 
   try {
     const decoded = jwt.verify(token, secretKey);
--- middleware/rateLimitMiddleware.js
+++ middleware/rateLimitMiddleware.js
@@ -4,7 +4,7 @@ const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
   windowMs: 30 * 1000, // 30 seconds
   max: 5,              // Limit each IP to 5 requests per windowMs
-  message: 'Too many requests, please try again later.',
+  message: 'I am ISMIS, cannot handle too many requests. Try again later.',
   standardHeaders: true,
   legacyHeaders: false,
 });
